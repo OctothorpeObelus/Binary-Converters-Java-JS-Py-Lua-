7b1649fc05fe7def6017e162a808611ed4e0caee
Michael S <OctothorpeObelus@users.noreply.github.com>
2017-01-23 09:38:20 -0500

    Create Decimal-Binary.java
    Buggy, some weird incomplete binary strands show up, needs some fixing.

---
+++ java/Decimal-Binary.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+class Main{
+
+     public static void main (String str[]) throws IOException {
+
+         // c 2017 Michael Smith
+         
+         Scanner scan = new Scanner(System.in);
+         int decimal = scan.nextInt();
+         String binary = "";
+         int[] bytes = { 128, 64, 32, 16, 8, 4, 2, 1 };
+         while ( decimal > 0 ) {
+         for (int i = 0; i < bytes.length; i++) {
+           if (decimal < 256) {
+             if (decimal >= bytes[i]) {
+               decimal -= bytes[i];
+                 binary += "" + 1 + "";
+             } else {
+                 binary +="" + 0 + "";
+             }
+           } else {
+               decimal -= 255;
+               binary += "" + 11111111 + " ";
+           }
+         }
+         System.out.println(binary);
+       }
+       
+     }
+
+}
